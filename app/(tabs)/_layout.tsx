ba47b1001c3db25ea7dc90a9c95a3cdf9f72a4ae
Raju sivanantham <rajusivanantham@gmail.com>
2025-11-13 17:44:20 -0500

    made a header and a tab menu

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,40 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from "expo-router";
 
-export default function RootLayout() {
+export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home'}} />
-      <Tabs.Screen name="about" options={{ title: 'About'}} />
+    <Tabs
+    screenOptions={{
+      tabBarActiveTintColor: '#ffd33d', //colour of tab bar selections (yellow)
+      headerStyle: {
+        backgroundColor: '#25292e', //background colour of the top bar (black)
+      },
+      headerShadowVisible: false, // The shadow of the header is turned off
+      headerTintColor: '#fff', //text colour of the header (whte)
+      tabBarStyle: {
+        backgroundColor: '#25292e', // background colour of the bottom tab menu (black)
+      },
+    }}
+  >
+      <Tabs.Screen
+      name="index"
+      options={{
+        title: 'Home',
+        tabBarIcon: ({ color, focused }) => (
+          <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+        ),
+      }}
+    />
+    <Tabs.Screen
+    name="about"
+    options= {{
+      title: 'About',
+      tabBarIcon: ({ color, focused }) => (
+        <Ionicons name={focused ? 'information-circle' : 'information-circle-outline'} color={color} size={24}/>
+      ),
+    }}
+  />
+
     </Tabs>
   );
 }
