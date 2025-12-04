8b017aecec9a1bed5029b625eb9256629b3e6692
Raju sivanantham <rajusivanantham@gmail.com>
2025-12-04 16:57:01 -0500

    made home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,18 +3,24 @@
 // and StyleSheet (our tool for styling, like CSS).
 
 // I am importing from the toolbox call react-native that holds all these components.
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+
+const PlaceholderImage = require('@/assets/images/background-image.png')
 
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href= "/about" style={styles.button}>
-        Go to About screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View>
+        <Button theme="primary" label= "Choose a photo" />
+        <Button label= "Use this photo" />
+      </View>
     </View>
   );
 }
@@ -22,17 +28,14 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e', //This makes the text like a light lavender colour
+    backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  }, 
+  imageContainer: {
+    flex: 1,
   },
-  text: {
-    color: '#fff', 
+  footerContainer: {
+    flex: 1/3,
+    alignItems: 'center',
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff'
-  },
-
-  });
+});
